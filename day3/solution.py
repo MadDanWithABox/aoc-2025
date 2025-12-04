09f5e295514607cec124db2e11c25da85dfe86f7
Dan <valhallaflash@gmail.com>
2025-12-04 15:52:45 +0000

    feat: add solution for day3 part1

--- day3/solution.py
+++ day3/solution.py
@@ -8,11 +8,24 @@ TEST_DATA = """987654321111111
 def get_banks(data_str:str):
     battery_banks = data_str.split("\n")
     print(f"there are {len(battery_banks)} battery banks")
+    return battery_banks
 
 def get_biggest_values(battery_bank: str):
     """
     Given a string representation of a battery bank, i.e. "1234", returns the largest joltage possible
     Returns: int """
+    jolt_list = [int(digit) for digit in battery_bank]
+
+    biggest_joltage = max(jolt_list[:-1])
+    prim_index = jolt_list.index(biggest_joltage)
+    sec_joltage = None
+    sec_index = None
+    for i, joltage in enumerate(jolt_list):
+        if sec_joltage is None or joltage >= sec_joltage:
+            if i > prim_index:
+                sec_joltage = joltage
+                sec_index = i
+    return int(f"{biggest_joltage}{sec_joltage}")
 
 def part1(input_data: str| None):
     joltages = []
@@ -20,6 +33,10 @@ def part1(input_data: str| None):
         input_data = TEST_DATA
     bat_banks = get_banks(input_data)
     for bank in bat_banks:
+        joltages.append(get_biggest_values(bank))
+    return sum(joltages)
+        
+
 
 
 
