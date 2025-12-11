a79fa14291e1471ed68e898f65acb4e047ec367b
Dan <valhallaflash@gmail.com>
2025-12-11 11:11:35 +0000

    feat: day 6 templates

---
+++ day6/solution.py
@@ -0,0 +1,27 @@
+from wrapper import aoc_runner
+
+TEST_DATA = """
+"""
+
+def parse(data_str):
+    "splits input data string into a 2D vector of rolls and gaps"
+    rows = data_str.split("\n")
+    
+
+
+def part1(input_data: str | None) -> int:
+    if input_data is None:
+        input_data = TEST_DATA
+
+
+        
+
+
+
+def part2(input_data: str | None) -> int:
+    if input_data is None:
+        input_data = TEST_DATA
+
+
+if __name__ == "__main__":
+    aoc_runner(part1, part2)
